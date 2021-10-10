b7b8046acf2aa706ae52e13d8848a22c0cd40d70
Lundi Mapundu <mapundu17@gmail.com>
2021-10-10 21:02:34 +0200

    Initial commit

---
+++ AmeliorateAegis/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmeliorateAegis.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [ForeignKey(nameof(CentreId))]
+        public Centre Centre { get; set; }
+        public long? CentreId { get; set; }
+    }
+}
